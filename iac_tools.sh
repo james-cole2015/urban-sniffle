9ad1e0274c5648c774539bdc358af63b76b7016c
james-cole2015 <brooks.houck@gmail.com>
2022-07-11 11:10:32 -0400

    creating iac_tools.sh

---
+++ iac_tools.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+##Installing Terraform
+sudo apt-get update && sudo apt-get install -y gnupg software-properties-common curl
+curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
+sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+sudo apt-get update && sudo apt-get install terraform
+terraform --version
+
+##Installing AWS CLI
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+aws --version 
