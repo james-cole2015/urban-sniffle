cbf36192791cbde0996b7c4fdb1fedec0bf7f2c5
james-cole2015 <brooks.houck@gmail.com>
2022-07-11 21:42:14 -0400

    Update iac_tools.sh

--- iac_tools.sh
+++ iac_tools.sh
@@ -5,10 +5,13 @@ sudo apt-get update && sudo apt-get install -y gnupg software-properties-common 
 curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
 sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
 sudo apt-get update && sudo apt-get install terraform
-terraform --version
+
 
 ##Installing AWS CLI
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 sudo ./aws/install
+
+#Verifying installations
 aws --version 
+terraform --version
