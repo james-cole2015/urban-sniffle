8754487cb7f68a1585ea81d4bc322ae2147da5a9
james-cole2015 <brooks.houck@gmail.com>
2022-07-27 08:41:43 -0400

    Update iac_tools.sh

--- iac_tools.sh
+++ iac_tools.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+apt install curl unzip -y
+
 ##Installing Terraform
 sudo apt-get update && sudo apt-get install -y gnupg software-properties-common curl
 curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
