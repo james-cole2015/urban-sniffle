932339ab4b6ab3bf4466c0acba5405f2da59ac78
brooks <brooks.houck@gmail.com>
2022-07-30 09:50:16 -0400

    adding script for jenkins asg

---
+++ jenkins_asg.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+apt-get update
+apt install curl unzip -y
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+
+sudo apt-get update && sudo apt-get install -y gnupg software-properties-common curl
+curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
+sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+sudo apt-get update && sudo apt-get install terraform
+
+apt install default-jre -y
+apt install default-jdk -y
+#verifying installs
+java -version
+javac -version
+
+#installing jenkins
+wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | gpg --dearmor -o /usr/share/keyrings/jenkins.gpg
+sh -c 'echo deb [signed-by=/usr/share/keyrings/jenkins.gpg] http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+apt update
+apt install jenkins -y
+systemctl start jenkins
