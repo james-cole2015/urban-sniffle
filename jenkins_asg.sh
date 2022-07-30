edb20a98e38f1234189d271ee936c571e4a3bfdb
brooks <brooks.houck@gmail.com>
2022-07-30 09:55:31 -0400

    adding -y to a few lines

--- jenkins_asg.sh
+++ jenkins_asg.sh
@@ -7,8 +7,8 @@ sudo ./aws/install
 
 sudo apt-get update && sudo apt-get install -y gnupg software-properties-common curl
 curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
-sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
-sudo apt-get update && sudo apt-get install terraform
+sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main" -y
+sudo apt-get update && sudo apt-get install terraform -y
 
 apt install default-jre -y
 apt install default-jdk -y
