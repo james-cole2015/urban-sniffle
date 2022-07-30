b62fc292bf94906ec56aaec26399b97a5f55f32d
brooks <brooks.houck@gmail.com>
2022-07-30 09:59:25 -0400

    updating with -y flags

--- iac_tools.sh
+++ iac_tools.sh
@@ -5,8 +5,8 @@ apt install curl unzip -y
 ##Installing Terraform
 sudo apt-get update && sudo apt-get install -y gnupg software-properties-common curl
 curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
-sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
-sudo apt-get update && sudo apt-get install terraform
+sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main" -y
+sudo apt-get update && sudo apt-get install terraform -y
 
 
 ##Installing AWS CLI
