7d87d2e16c8283ed25d258a165ca6eabe97fab92
james-cole2015 <brooks.houck@gmail.com>
2022-08-18 16:01:54 -0400

    Update iac_tools.sh

--- iac_tools.sh
+++ iac_tools.sh
@@ -11,6 +11,12 @@ sudo apt-get update && sudo apt-get install terraform -y
 ##Installing VIM RC
 curl https://raw.githubusercontent.com/linuxacademy/content-hashicorp-packer/master/env/vimrc -o ~/.vimrc
 
+##Installing Packer Plugins for VIM
+mkdir -p ~/.vim/pack/jvirtanen/start
+cd .vim/pack/jvirtanen/start
+git clone https://github.com/jvirtanen/vim-hcl.git
+
+
 ##Installing AWS CLI
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
