361ce71b5ce732554b5ce2ddfd6a663228a808c8
james-cole2015 <brooks.houck@gmail.com>
2022-08-18 16:02:18 -0400

    Update iac_tools.sh

--- iac_tools.sh
+++ iac_tools.sh
@@ -8,6 +8,11 @@ curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
 sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main" -y
 sudo apt-get update && sudo apt-get install terraform -y
 
+##Installing AWS CLI
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+
 ##Installing VIM RC
 curl https://raw.githubusercontent.com/linuxacademy/content-hashicorp-packer/master/env/vimrc -o ~/.vimrc
 
@@ -17,10 +22,7 @@ cd .vim/pack/jvirtanen/start
 git clone https://github.com/jvirtanen/vim-hcl.git
 
 
-##Installing AWS CLI
-curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-unzip awscliv2.zip
-sudo ./aws/install
+
 
 #Verifying installations
 aws --version 
