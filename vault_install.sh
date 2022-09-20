fe1305f18f020fdd620f7a83ef7291772b14e9f2
james-cole2015 <brooks.houck@gmail.com>
2022-09-19 22:19:22 -0400

    creating vault_install.sh

---
+++ vault_install.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+## installing Vault 
+
+curl --silent -Lo /tmp/vault.zip https://releases.hashicorp.com/vault/1.11.3/vault_1.11.3_linux_amd64.zip 
+cd /tmp
+sudo unzip vault.zip 
+sudo mv vault /usr/local/bin 
