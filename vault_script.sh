9170b67855c8ad85ee9bbbcbb2f65e840e466c72
brooks <brooks.houck@gmail.com>
2022-09-22 21:45:54 +0000

    updating script

--- vault_script.sh
+++ vault_script.sh
@@ -5,6 +5,7 @@
 #!/bin/bash
 apt-get update -y 
 apt install unzip -y
+mkdir vault_tmp
 curl https://releases.hashicorp.com/vault/1.11.3/vault_1.11.3_linux_amd64.zip > vault_tmp/vault.zip
 unzip vault_tmp/vault.zip
 mv vault /usr/local/bin
