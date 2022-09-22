f40f45fd0e716cbd49272c69abe0a1d5f9220c11
brooks <brooks.houck@gmail.com>
2022-09-22 22:09:15 +0000

    updating vimrc script

--- vimrc_install.sh
+++ vimrc_install.sh
@@ -3,6 +3,13 @@
 ######################
 
 #!/bin/bash
+## Pre-Reqs ##
+echo "Installing pre-reqs.."
+apt-get update -y 
+apt install curl unzip -y 
+
+## payload ##
+echo "installing vimrc profile"
 curl https://raw.githubusercontent.com/linuxacademy/content-hashicorp-packer/master/env/vimrc -o ~/.vimrc
 
 
