2efd06e3965c32895eecf5b2f15e44d93ad20c9e
james-cole2015 <brooks.houck@gmail.com>
2022-11-16 20:53:11 -0500

    Update awsCLI.sh

--- awsCLI.sh
+++ awsCLI.sh
@@ -3,7 +3,7 @@
 #########################
 
 #!/bin/bash
-apt install curl unzip -y
+sudo apt install curl unzip -y
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 sudo ./aws/install
